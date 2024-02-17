4a66785268ea13e230191f3ababaf4b9365c3ca2
RohiniAlla <133994292+RohiniAlla@users.noreply.github.com>
2024-02-16 22:34:11 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -41,9 +41,9 @@ api.init_app(app)
 
 load_dotenv()
 
-conn_str = os.getenv("couchbases://cb.3qbsphf8u9nk6u0p.cloud.couchbase.com")
-username = os.getenv("DB_Access")
-password = os.getenv("Srini@123")
+conn_str = "couchbases://cb.3qbsphf8u9nk6u0p.cloud.couchbase.com"
+username = "DB_Access"
+password = "Srini@123"
 
 if conn_str is None:
     print("DB_CONN_STR environment variable not set")
